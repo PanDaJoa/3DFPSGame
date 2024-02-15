164863b36e56294b8e1bd85fbd8a9ce271217d99
PanDaJoa <jms3590743@gmail.com>
2024-02-15 17:52:25 +0900

    풀링적용

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,59 +7,73 @@ using UnityEngine.UI;
 public class PlayerFire : MonoBehaviour
 {
     // 목표: 마우스 오른쪽 버튼을 누르면 시선이 바라보는 방향으로 수류탄을 던지고 싶다.
-    // 필요 속성
-    // - 수류탄 프라팹
-    public GameObject BombPrefeb;
+    // 필요 속성:
+    // - 수류탄 프리팹
+    public GameObject BombPrefab;
     // - 수류탄 던지는 위치
     public Transform FirePosition;
     // - 수류탄 던지는 파워
     public float ThrowPower = 15f;
 
-    [Header("Bomb카운터 UI")]
-    public Text BombScoreTextUI;
-    public int BombScore;
-    public const int MaxBombScore = 3;
+    // 폭탄 개수 3개로 제한
+    public int BombRemainCount;
+    public int BombMaxCount = 3;
+    // UI 위에 text로 표시하기 (ex. 1/3)
+    public Text BombTextUI;
 
+    // 실습 과제 10. 폭탄에 오브젝트 풀링(창고) 적용
+    public List<GameObject> BombPool; // 폭탄 창고
+    public int BombPoolSize = 5;
 
     private void Start()
     {
-        BombScore = MaxBombScore;
+        // 폭탄 창고 생성
+        BombPool = new List<GameObject>();
+        for (int i = 0; i < BombPoolSize; i++) // 생성할 폭탄 개수 만큼 반복
+        {
+            GameObject bombObject = Instantiate(BombPrefab); // 1. 생성
+            bombObject.SetActive(false);                     // 2. 비활성화
+            BombPool.Add(bombObject);                        // 3. 창고에 집어 넣는다.
+        }
+
+        BombRemainCount = BombMaxCount;
 
         RefreshUI();
     }
 
     private void RefreshUI()
     {
-        // UI 위에 Text로 표시하기;
-        BombScoreTextUI.text = $"{BombScore + " / " + MaxBombScore}";
+        BombTextUI.text = $"{BombRemainCount}/{BombMaxCount}";
     }
 
     private void Update()
     {
         /* 수류탄 투척 */
-        // 1. 마우스 오른쪽 버튼을 감지
-        if (Input.GetMouseButtonDown(1))
+        // 1. 마우스 오른쪽 버튼을 눌렀을 때 && 수류탄 개수가 0보다 크면
+        if (Input.GetMouseButtonDown(1) && BombRemainCount > 0)
         {
-            // 수류탄 개수가 0보다 큰 경우에만 던질 수 있음
-            if (BombScore > 0)
+            BombRemainCount--;
+
+            RefreshUI();
+
+            // 2. 창고에서 수류탄을 꺼낸 다음 던지는 위치로 조절
+            GameObject bomb = null;
+            for (int i = 0; i < BombPool.Count; ++i)        // 1. 창고를 뒤진다.
             {
-
-                // 수류탄 개수 감소
-                BombScore--;
-
-                RefreshUI();
-
-                // 2. 수류탄 던지는 위치에다가 수류탄 생성
-                GameObject bomb = Instantiate(BombPrefeb);
-                bomb.transform.position = FirePosition.position;
-
-                // 3. 시선이 바라보는 방향(카메라가 바라보는 방향 = 카라의 전방) 으로 수류탄 투척
-                Rigidbody rigidbody = bomb.GetComponent<Rigidbody>();
-                rigidbody.AddForce(Camera.main.transform.forward * ThrowPower, ForceMode.Impulse);
+                if (BombPool[i].activeInHierarchy == false) // 2. 쓸만한 폭탄을 찾는다.
+                {
+                    bomb = BombPool[i];
+                    bomb.SetActive(true);                   // 3. 꺼낸다.
+                    break;
+                }
             }
+
+            bomb.transform.position = FirePosition.position;
+
+            // 3. 시선이 바라보는 방향(카메라가 바라보는 방향 = 카메라의 전방) 으로 수류탄 투척
+            Rigidbody rigidbody = bomb.GetComponent<Rigidbody>();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Camera.main.transform.forward * ThrowPower, ForceMode.Impulse);
         }
-
-
-       
     }
 }
--- Assets/02.Scripts/Player/PlayerRotate.cs
+++ Assets/02.Scripts/Player/PlayerRotate.cs
@@ -8,21 +8,29 @@ public class PlayerRotate : MonoBehaviour
     // 필요 속성:
     // - 회전 속도
     public float RotationSpeed = 200; // 초당 200도까지 회전 가능한 속도
-    // 누적할 x각도와 y각도
+    // 누적할 x각도
     private float _mx = 0;
-    //private float _my = 0;
 
     void Update()
     {
-        // 순서:
+        if (!CameraManager.Focus)
+        {
+            return;
+        }
+
         // 1. 마우스 입력(drag) 받는다.
         float mouseX = Input.GetAxis("Mouse X");
 
-        // 2. 마우스 입력 값을 이용해 회전 방향을 구한다.
+        // 2. 마우스 입력 값만큼 x값을 누적한다.
         _mx += mouseX * RotationSpeed * Time.deltaTime;
-        //_mx = Mathf.Clamp(value: _mx, min: -270f, max: 270f);
-        
-        // 3. 회전 방향 회전한다.
-        transform.eulerAngles = new Vector3(0f, _mx, 0f);
+        //_mx = Mathf.Clamp(_mx, -270f, 270f);
+
+        // 3. 누적한 값에 따라 회전한다.
+        transform.eulerAngles = new Vector3(0f, _mx, 0);
+    }
+
+    public void ResetX()
+    {
+        _mx = 0;
     }
 }
