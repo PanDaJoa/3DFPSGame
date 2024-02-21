2fee6d6f7641206e4c794ce833921f09e3ab573f
PanDaJoa <jms3590743@gmail.com>
2024-02-21 19:12:41 +0900

    실습 과제 31. 몬스터가 죽으면 아이템이 드랍(Health: 20%, Stamina: 20%, Bullet:10%)

--- Assets/02.Scripts/Monster/Monster.cs
+++ Assets/02.Scripts/Monster/Monster.cs
@@ -10,12 +10,17 @@ public class Monster : MonoBehaviour, IHitable
     public float MaxHealth = 100;
     public Slider HealthSliderUI;
 
+    public GameObject ItemPrefabHealth;
+    public GameObject ItemPrefabStamina;
+    public GameObject ItemPrefabBullet;
+
     public void Hit(int damage)
     {
         Health -= damage;
         if (Health <= 0) 
         {
             Destroy(gameObject);
+            MakeItem();
         }
     }
     public void Init()
@@ -31,5 +36,32 @@ public class Monster : MonoBehaviour, IHitable
     void Update()
     {
         HealthSliderUI.value = Health / MaxHealth;
+    }
+    public void MakeItem()
+    {
+        int randomvalue = Random.Range(0, 10);
+        Debug.Log(randomvalue);
+        // 목표: 50% 확률로 체력 올려주는 아이템, 50% 확률로 이동속도 올려주는 아이템
+        if (randomvalue < 2)
+        {
+            // - 체력을 올려주는 아이템만들기
+            GameObject item = Instantiate(ItemPrefabHealth);
+            // - 위치를 나의 위치로 수정
+            item.transform.position = this.transform.position;
+        }
+        else if(randomvalue < 4)
+        {
+            // - 스태미나를 올려주는 아이템만들기
+            GameObject item = Instantiate(ItemPrefabStamina);
+            // - 위치를 나의 위치로 수정
+            item.transform.position = this.transform.position;
+        }
+        else if (randomvalue < 5)
+        {
+            // - 총알을 올려주는 아이템만들기
+            GameObject item = Instantiate(ItemPrefabBullet);
+            // - 위치를 나의 위치로 수정
+            item.transform.position = this.transform.position;
+        }
     }
 }
